4bcc8f6935eed029129389ebc1bcc191bfc63c8e
OrewellianWinston <108122899+OrewellianWinston@users.noreply.github.com>
2024-09-19 19:05:48 +0300

    Выполнил задачу: Добавьте в проект еще один класс – наследник класса DataReader, который должен обрабатывать входной файл определенного формата (согласно индивидуальному варианту, см. таблицу). Составьте модульные тесты для методов этого класса, постарайтесь покрыть тестами максимально возможный объем кода. Для работы с этим заданием создайте новую ветку кода на основе главной и фиксируйте в нее весь программный код в процессе разработки. Добейтесь выполнения всех тестов проекта, после чего объедините текущую ветку кода с главной.

---
+++ test/test_JSONDataReader.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from src.Types import DataType
+from src.JSONDataReader import JSONReader
+import pytest
+import json
+
+
+class TestJSONReader:
+    @pytest.fixture()
+    def json_file_content(self) -> tuple[dict[str, dict[str, int]], DataType]:
+        json_data = {
+            "Иванов Иван Иванович": {
+                "математика": 67,
+                "литература": 100,
+                "программирование": 91
+            },
+            "Петров Петр Петрович": {
+                "математика": 78,
+                "химия": 87,
+                "социология": 61
+            }
+        }
+        data = {
+            "Иванов Иван Иванович": [
+                ("математика", 67),
+                ("литература", 100),
+                ("программирование", 91)
+            ],
+            "Петров Петр Петрович": [
+                ("математика", 78),
+                ("химия", 87),
+                ("социология", 61)
+            ]
+        }
+        return json_data, data
+
+    def test_read(self, tmpdir, json_file_content: tuple[dict[str, dict[str, int]], DataType]) -> None:
+        json_data, expected_data = json_file_content
+
+        json_file = tmpdir.join("students.json")
+        with open(json_file, 'w', encoding='utf-8') as f:
+            json.dump(json_data, f, ensure_ascii=False)
+        reader = JSONReader()
+        students = reader.read(str(json_file))
+        assert students == expected_data
